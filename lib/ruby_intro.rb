802a0aaaa4f889391cfaea21614fd0aebe653e25
Jonathan Arauco <jonathanearauco@gmail.com>
2018-09-11 20:31:24 -0600

    HW 1 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,98 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum (arr)
+  sum = 0
+  arr.each { |elem| sum+=elem }
+  return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+
+def max_2_sum (arr)
+  if arr.empty?
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+  arr = arr.sort {|x,y| y <=> x}
+  return arr[0] + arr[1]  
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? (arr, n)
+  if arr.empty?
+    return false
+  else
+    for num1 in arr
+      for num2 in arr
+        if num1 + num2 == n && num1 != num2 
+          return true
+        end
+      end
+    end
+
+    return false
+  end
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  if(['a','e','i','o','u','A','E','I','O','U'].include? s[0])
+    return false
+  elsif s.empty?
+    return false
+  elsif !((s.ord >= 65 && s.ord <= 90)  || (s.ord >= 97 && s.ord <= 122))
+    return false      
+  else
+    return true
+  end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+  s = s.reverse
+
+  #Valid Bit String?
+  s.each_char do |c|
+    if !(c == '0' || c == '1')
+      return false;
+    end
+  end
+
+  if(s[0] == '0' && s[0] == '0')
+    return true
+  else
+    return false
+  end
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(_isbn, _price)
+    
+    if _isbn.empty?
+      raise ArgumentError
+    else
+      @isbn = _isbn
+    end
+
+    if  _price <= 0
+      raise ArgumentError
+    else
+      @price = _price
+    end
+  end
+  
+  def price_as_string
+    return "$" + '%.2f' % price.to_s
+  end  
+
 end
